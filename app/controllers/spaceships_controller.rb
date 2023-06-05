149bd632abd4a462e8e3873611b52c7a9dbc576a
Max Farhadi <max.farhadi@gmail.com>
2023-06-05 15:27:30 +0200

    created view files, footer, controllers

---
+++ app/controllers/spaceships_controller.rb
@@ -0,0 +1,23 @@
+class SpaceshipsController < ApplicationController
+  def index
+    @spaceships = Spaceship.all
+  end
+
+  def new
+    @spaceship = Spaceship.new
+  end
+
+  def create
+    @spaceship = Spaceship.new(spaceship_params)
+    @spaceship.save
+    redirect_to spaceship_path(@spaceship)
+  end
+
+  private
+
+  def spaceship_params
+    params.require(:spaceship).permit(:name, :category, :location)
+  end
+
+
+end
